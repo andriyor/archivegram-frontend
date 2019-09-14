9fdc3dec1a0510708ab802699db2e4e0e410a0b1
Andriy Orehov <andriyorehov@gmail.com>
2019-09-14 16:45:48 +0300

    add ability to search and unsubscribe from user

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import debounce from "lodash/debounce";
 
   import List, {
     Item,
@@ -13,75 +14,120 @@
   import Button, { Label } from "@smui/button";
   import Icon from "@smui/textfield/icon";
 
-  let userName = "";
-
-  let optionsTwoLine = [];
+  let searchUserName = "";
+  let subscribedInstagramUser = [];
+  let instagramUsersSearchResult = [];
 
   onMount(() => {
-    fetchUsers();
+    fetchUsers().then(
+      subscribedUsers => (subscribedInstagramUser = subscribedUsers)
+    );
   });
 
   function fetchUsers() {
-    fetch("http://localhost:3000/users", { method: "GET", mode: "cors" })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        optionsTwoLine = myJson["users"];
-        console.log(myJson);
-      });
+    return fetch("http://localhost:3000/users", { method: "GET", mode: "cors" })
+      .then(response => response.json())
+      .then(myJson => myJson["users"]);
   }
 
-  function sendUsername() {
-    console.log(userName);
-    fetch("http://localhost:3000/users", {
-      method: "POST", // or 'PUT'
-      body: JSON.stringify({ user: { username: userName } }), // data can be `string` or {object}!
+  function sendUser(username) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      body: JSON.stringify(username),
       headers: {
         "Content-Type": "application/json"
       }
-    });
+    }).then(response => response.json());
   }
 
-  function handlePress(event) {
-    console.log("kek");
-    if (event.key === "Enter") {
-      sendUsername();
+  function serchIntagramUser(username) {
+    return fetch(`http://localhost:3000/instagram-user?username=${username}`)
+      .then(response => response.json())
+      .then(myJson => myJson["users"]);
+  }
+
+  const handleInput = debounce(event => {
+    let username = event.target.value;
+    if (username === "") {
+      instagramUsersSearchResult = [];
+    } else {
+      serchIntagramUser(username).then(
+        usernames => (instagramUsersSearchResult = usernames)
+      );
     }
+  }, 500);
+
+  function selectInstagramUser(instagramUser) {
+    sendUser(instagramUser).then(user => subscribedInstagramUser = [user, ...subscribedInstagramUser]);
+    instagramUsersSearchResult = [];
+    searchUserName = "";
+  }
+
+  function removeUser(userPk) {
+    fetch(`http://localhost:3000/users/${userPk}`, { method: "DELETE" });
+    subscribedInstagramUser = subscribedInstagramUser.filter(
+      user => user.pk !== userPk
+    );
   }
 </script>
 
 <style>
-
+  * :global(.avatarsearch) {
+    height: 40px;
+  }
+  * :global(.search-list) {
+    height: 150px;
+    overflow: auto;
+  }
+  * :global(.search-list-wrapper) {
+    width: 30%;
+  }
 </style>
 
 <Textfield
   withLeadingIcon
-  bind:value={userName}
-  on:keydown={handlePress}
+  on:input={handleInput}
+  bind:value={searchUserName}
   label="Username"
   input$aria-controls="helper-text-standard-b"
   input$aria-describedby="helper-text-standard-b">
   <Icon class="material-icons">playlist_add</Icon>
 </Textfield>
-<Button on:click={sendUsername}>
-  <Icon class="material-icons">add</Icon>
-  <Label>Add user</Label>
-</Button>
 
-<List class="demo-list" twoLine avatarList singleSelection>
-  {#each optionsTwoLine as item}
-    <Item>
-      <Graphic
-        style="background-image: url(https://via.placeholder.com/40x40.png?text={item.fullname
-          .split(' ')
-          .map(val => val.substring(0, 1))
-          .join('')});" />
-      <Text>
-        <PrimaryText>{item.fullname}</PrimaryText>
-        <SecondaryText>{item.username}</SecondaryText>
-      </Text>
-      <Meta class="material-icons">info</Meta>
-    </Item>
-  {/each}
-</List>
+{#if instagramUsersSearchResult.length}
+  <div class="search-list-wrapper">
+    <List class="search-list" dense avatarList>
+      {#each instagramUsersSearchResult as instagramUser}
+        <Item
+          class="avatarsearch"
+          on:SMUI:action={() => selectInstagramUser(instagramUser)}>
+          <Graphic
+            style="background-image: url({instagramUser.profile_pic_url});
+            background-size: 35px;" />
+          <Text>{instagramUser.full_name}</Text>
+        </Item>
+      {/each}
+    </List>
+  </div>
+{/if}
+
+{#if subscribedInstagramUser.length}
+  <List class="demo-list" twoLine avatarList singleSelection>
+    {#each subscribedInstagramUser as subscribedUser}
+      <Item>
+        <Graphic
+          style="background-image: url({subscribedUser.profile_pic_url}); ;
+          background-size: 40px;" />
+        <Text>
+          <PrimaryText>{subscribedUser.full_name}</PrimaryText>
+          <SecondaryText>{subscribedUser.username}</SecondaryText>
+        </Text>
+        <Meta
+          class="material-icons"
+          on:click={() => removeUser(subscribedUser.pk)}>
+          remove
+        </Meta>
+      </Item>
+    {/each}
+  </List>
+{/if}
