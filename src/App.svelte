12b7a082a431df02370fe6560f485de8ee8260cf
Andriy Orehov <andriyorehov@gmail.com>
2019-09-15 13:19:52 +0300

    add ability to download and show stories

--- src/App.svelte
+++ src/App.svelte
@@ -22,12 +22,22 @@
     fetchUsers().then(
       subscribedUsers => (subscribedInstagramUser = subscribedUsers)
     );
+    fetchStories().then(st => {
+      stories.update(st);
+    });
   });
 
   function fetchUsers() {
     return fetch("http://localhost:3000/users", { method: "GET", mode: "cors" })
       .then(response => response.json())
       .then(myJson => myJson["users"]);
+  }
+
+  function fetchStories() {
+    return fetch("http://localhost:3000/stories", {
+      method: "GET",
+      mode: "cors"
+    }).then(response => response.json());
   }
 
   function sendUser(username) {
@@ -58,7 +68,9 @@
   }, 500);
 
   function selectInstagramUser(instagramUser) {
-    sendUser(instagramUser).then(user => subscribedInstagramUser = [user, ...subscribedInstagramUser]);
+    sendUser(instagramUser).then(
+      user => (subscribedInstagramUser = [user, ...subscribedInstagramUser])
+    );
     instagramUsersSearchResult = [];
     searchUserName = "";
   }
@@ -69,6 +81,92 @@
       user => user.pk !== userPk
     );
   }
+
+  let stories = new Zuck("story", {
+    skin: "snapgram", // container class
+    avatars: true, // shows user photo instead of last story item preview
+    list: false, // displays a timeline instead of carousel
+    openEffect: true, // enables effect when opening story - may decrease performance
+    cubeEffect: false, // enables the 3d cube effect when sliding story - may decrease performance
+    autoFullScreen: false, // enables fullscreen on mobile browsers
+    backButton: true, // adds a back button to close the story viewer
+    backNative: false, // uses window history to enable back button on browsers/android
+    previousTap: true, // use 1/3 of the screen to navigate to previous item when tap the story
+    localStorage: true, // set true to save "seen" position. Element must have a id to save properly.
+    reactive: true, // set true if you use frameworks like React to control the timeline (see react.sample.html)
+
+    stories: [],
+
+    callbacks: {
+      onOpen(storyId, callback) {
+        callback(); // on open story viewer
+      },
+
+      onView(storyId) {
+        // on view story
+      },
+
+      onEnd(storyId, callback) {
+        callback(); // on end story
+      },
+
+      onClose(storyId, callback) {
+        callback(); // on close story viewer
+      },
+
+      onNavigateItem(storyId, nextStoryId, callback) {
+        callback(); // on navigate item of story
+      },
+
+      onDataUpdate(currentState, callback) {
+        callback(); // use to update state on your reactive framework
+      }
+    },
+
+    template: {
+      // use these functions to render custom templates
+      // see src/zuck.js for more details
+
+      timelineItem(itemData) {
+        return ``;
+      },
+
+      timelineStoryItem(itemData) {
+        return ``;
+      },
+
+      viewerItem(storyData, currentStoryItem) {
+        return ``;
+      },
+
+      viewerItemPointer(index, currentIndex, item) {
+        return ``;
+      },
+
+      viewerItemBody(index, currentIndex, item) {
+        return ``;
+      }
+    },
+
+    language: {
+      // if you need to translate :)
+      unmute: "Touch to unmute",
+      keyboardTip: "Press space to see next",
+      visitLink: "Visit link",
+      time: {
+        ago: "ago",
+        hour: "hour",
+        hours: "hours",
+        minute: "minute",
+        minutes: "minutes",
+        fromnow: "from now",
+        seconds: "seconds",
+        yesterday: "yesterday",
+        tomorrow: "tomorrow",
+        days: "days"
+      }
+    }
+  });
 </script>
 
 <style>
@@ -125,7 +223,7 @@
         <Meta
           class="material-icons"
           on:click={() => removeUser(subscribedUser.pk)}>
-          remove
+          clear
         </Meta>
       </Item>
     {/each}
